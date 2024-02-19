1166f0ffab6796f36ee5272c8480e55ce2d422de
julian12100 <xfsanchez10@gmail.com>
2024-02-18 22:08:20 -0500

    Se corrige error en el tipo de valor del desplegable tipo de extension

--- src/components/ExtensionForm.jsx
+++ src/components/ExtensionForm.jsx
@@ -81,8 +81,8 @@ function ExtensionForm({ selectedExtension }) {
           className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
         >
           <option value="">Seleccionar Dispositivo</option>
-          <option value="Líneas corporativas">Virtual</option>
-          <option value="Servicios en salud">Fisico</option>
+          <option value="Virtual">Virtual</option>
+          <option value="Fisico">Fisico</option>
         </select>
       </div>
 
